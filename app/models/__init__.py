211e2c06e61f25216ac1aa712c9bb46e6924541d
Yunus Emre Çelikkıran <108741264+yunuse1@users.noreply.github.com>
2026-09-02 10:30:48 +0300

    Register user model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,11 @@
 from app.models.tenant import Tenant
 from app.models.widget import Widget
 from app.models.submission import Submission
+from app.models.user import User
 
 __all__ = [
     "Tenant",
     "Widget",
     "Submission",
+    "User",
 ]
