135f51a83144173d97401eb1379003b01ba6d7a5
Yunus Emre Çelikkıran <108741264+yunuse1@users.noreply.github.com>
2026-09-02 11:56:48 +0300

    feat: register notification job model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,10 +2,12 @@ from app.models.tenant import Tenant
 from app.models.widget import Widget
 from app.models.submission import Submission
 from app.models.user import User
+from app.models.notification_job import NotificationJob
 
 __all__ = [
     "Tenant",
     "Widget",
     "Submission",
     "User",
+    "NotificationJob",
 ]
